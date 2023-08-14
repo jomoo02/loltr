70d9692dc28eed898a38cc265707dcada4667e0a
jomoo02 <jomootgrn88@naver.com>
2023-08-14 16:53:07 +0900

    fix: MatchCard.vue 장신구 0번일 때 빈칸이랑 0번 둘다 보이는 현상 수정

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex justify-center mt-12 sm:mt-36">
-        <div class="flex w-full xs:w-9/12 px-2.5 xs:px-0 sm:w-1/3 h-10">
+        <div class="flex w-full xs:w-10/12 px-2.5 xs:px-0 sm:w-1/3 h-10">
             <SummonerNameInput></SummonerNameInput>
         </div>
     </div>
