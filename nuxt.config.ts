b411d29a09a35fec8df7f26efb1dab1903da6a5e
jomoo02 <jomootgrn88@naver.com>
2023-08-14 17:26:48 +0900

    feat: app head meta name: viewport 모바일 화면 확대 막기

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'loltr',
+      meta: [
+        { name: 'description', content: "lol관련 정보를 보여줍니다." },
+        { name:  'viewport', content: "width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" },
+      ]
+    }
+  },
   runtimeConfig: {
     MONGODB_URI: process.env.MONGODB_URI,
     API_KEY: process.env.API_KEY,
