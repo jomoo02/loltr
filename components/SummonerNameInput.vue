a942dac02904e405d4e64ac0dbec0d9012192d44
jomoo02 <jomootgrn88@naver.com>
2023-08-15 23:08:32 +0900

    design: SummonerNameInput.vue 수정2

--- components/SummonerNameInput.vue
+++ components/SummonerNameInput.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="inputcomponent h-full">
-        <div id="input_id" class="w-full h-full flex items-center border-2 rounded-lg border-zinc-200 bg-white inputcomponent" :class="focus === 1 ? 'border-b-0 rounded-b-none h-[30px]': ''">
+        <div id="input_id" class="w-full h-full flex items-center border-2 rounded-lg border-zinc-200 bg-white inputcomponent" :class="focus === 1 ? 'rounded-b-none': ''">
             <input type="text" placeholder="소환사명" :value="summonerName" @input="changeInputSummonerName"
             @keypress.enter="routeSummonerName" @focus="focusInputComponent"
-            class="w-full h-full px-5 text-xs xs:text-sm outline-none rounded-xl cursor-text" :class="focus === 1 ? 'rounded-b-none': ''">
-            <button class="h-full font-bold text-base xs:text-lg text-zinc-500 hover:bg-stone-200 px-2 rounded-md subpixel-antialiased"
+            class="w-full flex px-5  text-xs xs:text-sm outline-none rounded-xl cursor-text" :class="focus === 1 ? 'ed-b-roundnone': ''">
+            <button class="flex h-full font-bold text-base xs:text-lg text-zinc-500 hover:bg-stone-200 px-2 rounded-md subpixel-antialiased"
             @click="routeSummonerName">
                 lol.tr
             </button>
         </div>
         <ClientOnly>
-            <div class="w-full text-xs xs:text-sm rounded-b-xl border-2 border-b-2 border-zinc-200 bg-white" id="recentSearchList" :class="focus === 1 ? 'block' : 'hidden'">
+            <div class="w-full text-xs xs:text-sm rounded-b-xl border-x-2 border-b-2 border-zinc-200 bg-white" id="recentSearchList" :class="focus === 1 ? 'block' : 'hidden'">
                 <div v-if="summonerName === ''">
                     <ul v-if="mainStore.recentSearchSummonerList.length > 0">
                         <li v-for="(summoner, index) in mainStore.recentSearchSummonerList" 
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-center mt-12 sm:mt-36">
-        <div class="w-full flex justify-center">
-            <SummonerNameInput class=" xs:w-10/12 px-2.5 xs:px-0 sm:w-1/3 h-8" />
+        <div class="w-full flex justify-center h-8">
+            <SummonerNameInput class="xs:w-10/12 px-2.5 xs:px-0 sm:w-1/3" />
         </div>
     </div>
 </template>
